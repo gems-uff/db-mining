492aa5b88f48a076aa6d0252b2e4d589a55b29fc
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2019-08-29 12:48:42 -0700

    Issue #18: Adds script to clone/update repositories

---
+++ collection/download.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+import pandas as pd
+
+# File to load the data with repositories
+REPO_FILE = '../docs/annotated.xlsx'
+
+# Dir to clone/update repositories
+REPO_DIR = os.path.abspath('../repos')
+
+
+def main():
+    print(f'Loading repositories from {REPO_FILE}.')
+    df = pd.read_excel(REPO_FILE, keep_default_na=False)
+
+    print('Removing discarded repositories.')
+    df = df[df.discardReason == '']
+
+    total = len(df)
+    print(f'Cloning/updating {total} repositories...')
+
+    for i, row in df.iterrows():
+        print(f'Processing repository {row["owner"]}/{row["name"]}.')
+        source = f'https://github.com/{row["owner"]}/{row["name"]}.git'
+        target = REPO_DIR + os.sep + row['owner'] + os.sep + row['name']
+
+        if os.path.isdir(target):
+            os.chdir(target)
+            subprocess.run(['git', 'remote', 'update'])
+        else:
+            os.makedirs(target, exist_ok=True)
+            subprocess.run(['git', 'clone', source, target])
+
+        print('Ok ({:.0f}%).'.format((i + 1) / total * 100))
+
+        if i == 5:
+            break
+
+    print("\nFinished.")
+
+
+if __name__ == "__main__":
+    main()
