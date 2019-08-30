75c1e08504cf8e00c16e55076621716af3872a84
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2019-08-30 16:38:16 -0700

    Issue #18: Adds script to reset the working area

--- collection/reset.py
+++ collection/reset.py
@@ -11,6 +11,10 @@ REPO_DIR = os.path.abspath('../repos')
 
 
 def main():
+    """
+    This program can be useful to fix collisions (due to data migrated from case insensitive to case sensitive file
+    systems) and to update the workspace to the most recent version.
+    """
     print(f'Loading repositories from {REPO_FILE}.')
     df = pd.read_excel(REPO_FILE, keep_default_na=False)
 
@@ -18,22 +22,16 @@ def main():
     df = df[df.discardReason == '']
 
     total = len(df)
-    print(f'Cloning/updating {total} repositories...')
+    print(f'Checking out {total} repositories...')
 
     for i, row in df.iterrows():
         print(f'Processing repository {row["owner"]}/{row["name"]}.')
-        source = f'https://github.com/{row["owner"]}/{row["name"]}.git'
         target = REPO_DIR + os.sep + row['owner'] + os.sep + row['name']
 
-        if os.path.isdir(target):
-            os.chdir(target)
-            subprocess.run(['git', 'remote', 'update'])
-            subprocess.run(['git', 'reset', '--hard', '-q', 'origin/HEAD'])
-            subprocess.run(['git', 'clean', '-d', '-f', '-x', '-q'])
-            subprocess.run(['git', 'status', '-s'])
-        else:
-            os.makedirs(target, exist_ok=True)
-            subprocess.run(['git', 'clone', source, target])
+        os.chdir(target)
+        subprocess.run(['git', 'reset', '--hard', '-q', 'origin/HEAD'])
+        subprocess.run(['git', 'clean', '-d', '-f', '-x', '-q'])
+        subprocess.run(['git', 'status', '-s'])
 
         print('Ok ({:.0f}%).'.format((i + 1) / total * 100))
 
