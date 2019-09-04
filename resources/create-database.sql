b6938743f24f04ff54b6b27d73a5f7616da7c404
Vanessa Braganholo <vanessa@ic.uff.br>
2019-09-04 16:55:12 -0700

    Issue #19: Creates relational database to store project metadata, and script to populate the database from the excel file

--- resources/create-database.sql
+++ resources/create-database.sql
@@ -1,55 +1,99 @@
-CREATE DATABASE db-mining;
+CREATE TABLE IF NOT EXISTS language (
+   language_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name TEXT
+);
 
-CREATE TABLE project (
+CREATE TABLE IF NOT EXISTS domain (
+   domain_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS project (
    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
    owner TEXT,
    name TEXT,
-   language TEXT, 
-   domain TEXT
-); 
+   createdAt TEXT,
+   pushedAt TEXT,
+   isMirror BOOLEAN,
+   diskUsage INTEGER,
+   languages INTEGER,
+   contributors INTEGER,
+   watchers	INTEGER,
+   stargazers INTEGER,
+   forks INTEGER,
+   issues INTEGER,
+   commits INTEGER,
+   pullRequests INTEGER,
+   branches INTEGER,
+   tags INTEGER,
+   releases	INTEGER,
+   description TEXT,
+   language_id INTEGER,
+   domain_id INTEGER,
+   FOREIGN KEY (language_id) REFERENCES language (language_id) ON DELETE RESTRICT,
+   FOREIGN KEY (domain_id) REFERENCES domain (domain_id) ON DELETE RESTRICT
+);
 
-CREATE TABLE project_version (
+
+CREATE TABLE IF NOT EXISTS project_version (
    version_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-   version TEXT, 
+   sha1 TEXT,
    last BOOLEAN,
    project_id INTEGER,
    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE RESTRICT
 );
 
-CREATE TABLE database_type (
+CREATE TABLE IF NOT EXISTS database_type (
    type_id INTEGER PRIMARY KEY AUTOINCREMENT, 
    name TEXT
 );
 
-
-CREATE TABLE database (
+CREATE TABLE IF NOT EXISTS database (
    database_id INTEGER PRIMARY KEY AUTOINCREMENT,
    name TEXT, 
    type_id INTEGER,
    FOREIGN KEY (type_id) REFERENCES database_type (type_id) ON DELETE RESTRICT 
 );
 
-CREATE TABLE project_database (
-   project_id INTEGER NOT NULL, 
-   database_id INTEGER NOT NULL,
-   PRIMARY KEY (project_id, database_id),
-   FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE RESTRICT,
-   FOREIGN KEY (database_id) REFERENCES database (database_id) ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS query_strategy (
+   query_strategy_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name TEXT
 );
 
-CREATE TABLE strategy (
-   strategy_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-   type TEXT,
+CREATE TABLE IF NOT EXISTS implementation_strategy (
+   implementation_strategy_id INTEGER PRIMARY KEY AUTOINCREMENT,
    name TEXT
-); 
+);
 
-CREATE TABLE project_strategy (
-   project_id INTEGER NOT NULL, 
-   strategy_id INTEGER NOT NULL, 
-   PRIMARY KEY (project_id, strategy_id),
-   FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE RESTRICT,
-   FOREIGN KEY (strategy_id) REFERENCES strategy (strategy_id) ON DELETE RESTRICT
-);   
-   
+CREATE TABLE IF NOT EXISTS heuristic (
+   heuristic_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   regex TEXT,
+   type TEXT,
+   database_id INTEGER,
+   language_id INTEGER,
+   query_strategy_id INTEGER,
+   implementation_strategy_id INTEGER,
+   FOREIGN KEY (database_id) REFERENCES database (database_id) ON DELETE RESTRICT,
+   FOREIGN KEY (language_id) REFERENCES language (language_id) ON DELETE RESTRICT,
+   FOREIGN KEY (query_strategy_id) REFERENCES query_strategy (query_strategy_id) ON DELETE RESTRICT,
+   FOREIGN KEY (implementation_strategy_id) REFERENCES implementation_strategy (implementation_strategy_id) ON DELETE RESTRICT
+);
 
-   
+CREATE TABLE IF NOT EXISTS execution (
+   heuristic_id INTEGER,
+   project_version_id INTEGER,
+   validated BOOLEAN,
+   accepted BOOLEAN,
+   PRIMARY KEY (heuristic_id, project_version_id),
+   FOREIGN KEY (heuristic_id) REFERENCES heuristic (heuristic_id) ON DELETE RESTRICT,
+   FOREIGN KEY (project_version_id) REFERENCES project_version (project_version_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS match (
+   match_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   file TEXT,
+   chunk TEXT,
+   heuristic_id INTEGER,
+   project_version_id INTEGER,
+   FOREIGN KEY (heuristic_id, project_version_id) REFERENCES execution
+);
