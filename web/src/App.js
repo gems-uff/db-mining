39ced205ddf9a1a75eef4f16e1e3e76b976cc8b5
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2019-09-24 16:21:58 -0700

    Issue #21: Adds initial react app showing projects

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PersistentDrawerLeft from "./PersistentDrawerLeft";
+
+function App() {
+  return (
+    <div className="App">
+      <PersistentDrawerLeft />
+    </div>
+  );
+}
+
+export default App;
