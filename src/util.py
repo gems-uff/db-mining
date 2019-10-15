979352a0d7115e7df318d5e09bb8a5323f619425
Vanessa Braganholo <vanessa@ic.uff.br>
2019-10-15 10:54:18 -0700

    Issue #19: small updates to the database.py file

--- src/util.py
+++ src/util.py
@@ -23,7 +23,6 @@ VENUE_KEYS = RESOURCE_DIR + os.sep + 'venue_keys.txt'
 PAPERS_FILE = RESOURCE_DIR + os.sep + 'papers.xlsx'
 FILTERED_PAPERS_FILE = RESOURCE_DIR + os.sep + 'filtered_papers.xlsx'
 
-DATABASE_FILE = RESOURCE_DIR + os.sep + 'db-mining.db'
 DATABASE_CONFIG_FILE = RESOURCE_DIR + os.sep + 'database.json'
 
 def bold(string):
@@ -40,3 +39,6 @@ def green(string):
 
 def yellow(string):
     return '\033[33m' + string + '\033[0m'
+
+def get_database_uri(database_name):
+    return RESOURCE_DIR + os.sep + database_name
