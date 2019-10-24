f5c7c3c56dce8cd0afb0912af4c5d22cd4103580
Vanessa Braganholo <vanessa@ic.uff.br>
2019-10-24 11:20:26 -0700

    Issue #19: moves database and database configuration file to the root folder

--- src/util.py
+++ src/util.py
@@ -23,7 +23,7 @@ VENUE_KEYS = RESOURCE_DIR + os.sep + 'venue_keys.txt'
 PAPERS_FILE = RESOURCE_DIR + os.sep + 'papers.xlsx'
 FILTERED_PAPERS_FILE = RESOURCE_DIR + os.sep + 'filtered_papers.xlsx'
 
-DATABASE_CONFIG_FILE = RESOURCE_DIR + os.sep + 'database.json'
+DATABASE_CONFIG_FILE = BASE_DIR + os.sep + 'database.json'
 
 def bold(string):
     return '\033[1m' + string + '\033[0m'
@@ -41,4 +41,4 @@ def yellow(string):
     return '\033[33m' + string + '\033[0m'
 
 def get_database_uri(database_name):
-    return RESOURCE_DIR + os.sep + database_name
+    return BASE_DIR + os.sep + database_name
